9158907538bfdf4599119fe323013c02aedddb72
kioko <kiokotomas@gmail.com>
2016-02-06 15:41:35 +0300

    Add annotations, allow the user to add annotation by long pressing on the map view .

--- Blip Blip/ViewController.swift
+++ Blip Blip/ViewController.swift
@@ -32,11 +32,53 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         mapView.setRegion(mapRegion, animated: true)
         
+        //Add annotations
+        let annotation = MKPointAnnotation()
+        
+        //Annotaion Attributes
+        annotation.coordinate = location
+        annotation.title = "Ihub"
+        annotation.subtitle = "The Geek Dome"
+        
+        //add the annotation to the map
+        mapView.addAnnotation(annotation)
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: "gestureAction:")
+        
+        //set the minimum press duration in secs
+        longPressGesture.minimumPressDuration = 2
+        
+        //Add the guesture to the map
+        mapView.addGestureRecognizer(longPressGesture)
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Handles the guesture action. This allows the user to add annotations to 
+    // the map by long pressing the map on Long press.
+    func gestureAction(gestureRecognizer : UIGestureRecognizer){
+        
+        //Get the location of where the user has long pressed
+        let touchPoint = gestureRecognizer.locationInView(self.mapView)
+        
+        //convert the touch point to coordinates
+        let location : CLLocationCoordinate2D = mapView.convertPoint(touchPoint,
+            toCoordinateFromView: self.mapView)
+        
+        //Add the annotation to the map
+        let annotation = MKPointAnnotation()
+        
+        //Annotaion Attributes
+        annotation.coordinate = location
+        annotation.title = "Blip"
+        annotation.subtitle = "New location"
+        
+        //add the annotation to the map
+        mapView.addAnnotation(annotation)
     }
     
     
