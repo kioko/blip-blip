f2ee8b7c42523b06bb2b04f77bfea969a511c603
kioko <kiokotomas@gmail.com>
2016-02-06 20:46:03 +0300

    Create TableViewController class

---
+++ Blip Blip/TableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewController.swift
+//  Blip Blip
+//
+//  Created by Kioko on 05/02/2016.
+//  Copyright © 2016 Thomas Kioko. All rights reserved.
+//
+
+import UIKit
+
+
+
+class TableViewController: UITableViewController {
+
+   
+}
