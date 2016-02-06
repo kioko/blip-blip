943687a525c06d415e7ba3486e4cbb831cda7588
kioko <kiokotomas@gmail.com>
2016-02-06 15:20:04 +0300

    Add functionality to load map view and display pre-set location

--- Blip Blip/ViewController.swift
+++ Blip Blip/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude :CLLocationDegrees = -1.2921
+        let longitude :CLLocationDegrees = 36.8219
+        
+        
+        //Difference between one side of the screen ot the other/ Zoom in
+        let latDelata : CLLocationDegrees = 0.01
+        let longDelata : CLLocationDegrees = 0.01
+        
+        let span : MKCoordinateSpan = MKCoordinateSpanMake(latDelata, longDelata)
+        
+        let location : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let mapRegion : MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        mapView.setRegion(mapRegion, animated: true)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
